8f8a61e9617c86abecdecfd0c8560d56cbc9942b
Salman_JS <salman4javascript@gmail.com>
2023-02-06 23:21:12 +0530

    Fixed table view for params container

--- src/scripts/components/NavCrumbs/lang.js
+++ src/scripts/components/NavCrumbs/lang.js
@@ -2,6 +2,7 @@ export const crumbsLang = {
     loader: "Creating your code editor, please wait...",
     key: "Key",
     value: "Value",
+    controls: "Control",
     basic: "Basic Auth",
     checkbox: "Show Password",
     showMore: "+",
