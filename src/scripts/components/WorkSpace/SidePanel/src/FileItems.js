51abc3ee55137c1b696179dd812aabbd6e160cb2
Salman_JS <salman4javascript@gmail.com>
2023-02-15 22:31:01 +0530

    Added icons for the respective file

--- src/scripts/components/WorkSpace/SidePanel/src/FileItems.js
+++ src/scripts/components/WorkSpace/SidePanel/src/FileItems.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { IconFile, IconFolder } from '../../Icons/IconsHolder';
 
 const FileItems = (props) => {
   return (
     <div className = "brew-title-workspace file-items">
-      {props.name}
+     {props.isDirectory ? <IconFolder /> : <IconFile />}
+     {props.name}
     </div>
   )
 }
 
 export default FileItems;
+
+// brew-title-workspace file-items
