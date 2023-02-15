ff921a5dcba3ad959670e3b001d2e48083ab4721
Salman_JS <salman4javascript@gmail.com>
2023-02-16 00:12:24 +0530

    Hanlded click function on explorer

--- src/scripts/components/WorkSpace/SidePanel/src/FileItems.js
+++ src/scripts/components/WorkSpace/SidePanel/src/FileItems.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { IconFile, IconFolder } from '../../Icons/IconsHolder';
 
 const FileItems = (props) => {
+
+  // Test function!
+  function handleClick(names){
+    props.navigation(names);
+  }
+
+
   return (
-    <div className = "brew-title-workspace file-items">
+    <div className = "brew-title-workspace file-items" onClick={() => handleClick(props.isDirectory, props.name)}>
      {props.isDirectory ? <IconFolder /> : <IconFile />}
      {props.name}
     </div>
