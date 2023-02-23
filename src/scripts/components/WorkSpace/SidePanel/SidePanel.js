0e41b5914029b237dd34c7752ee774a78b48d9d9
Salman_JS <salman4javascript@gmail.com>
2023-02-23 19:05:05 +0530

    File creation initiated

--- src/scripts/components/WorkSpace/SidePanel/SidePanel.js
+++ src/scripts/components/WorkSpace/SidePanel/SidePanel.js
@@ -22,6 +22,8 @@ const SidePanel = (props) => {
 
   // New folder and value state handler!
   const [value, setValue] = useState();
+  const [folder, setFolder] = useState(false);
+  const [file, setFile] = useState(false);
   const [error, setError] = useState();
 
   // Get the current path of the working directory!;
@@ -89,7 +91,17 @@ const SidePanel = (props) => {
   }
 
   // Handle Toast Close!
-  function handleToast(){
+  function handleToast(module){
+
+    // Deciding the creation module!
+    if(module === "folder"){
+      setFolder(true);
+    } else if(module === "file") {
+      setFile(true);
+    } else {
+      setFolder(!folder);
+      setFile(!file);
+    }
     // Setting the error undefined to retain miniToast rendering on opening the panel for the first time after the error pops!
     setError(undefined);
     setToastShow(!toastShow);
@@ -111,10 +123,10 @@ const SidePanel = (props) => {
         // So, Whenever the folder has been created, initiating handleback function to get back to the original directory!
         handleBack(wd);
         // Close the creation toast dialog!
-        handleToast();
+        handleToast(); // State decide helper will be clear out in the stage!
       } else {
         // Throw error that the name must be needed to create a folder!
-        setError(workLang.error)
+        setError(workLang.errorFolder)
       }
     } catch (err) {
       console.error(err);
@@ -123,8 +135,29 @@ const SidePanel = (props) => {
 
   // Handle file creation!
   function fileCreation(){
-    console.log("Create file has been clicked!");
+    fs.appendFile(value, 'w', function(err, file){
+      if(err) {
+        // Handle the error!
+        setError(workLang.errorFile)
+      } else {
+        // This two function order should be the same as for some reason, JS and CSS file type files
+        // have been created outside of the targeted folder!
+        handleToast(); // State decide helper will be clear out in the stage!
+        handleBack(wd); // Value persistant!
+      }
+     })
   }
+
+
+  // Decide Creation!
+  function decideCreation(){
+    if(folder){
+      folderCreation();
+    } else if(file){
+      fileCreation();
+    }
+  }
+  
 
   // Constructor - Get all the files and folders in working directory before the component renders!
   useEffect(() => {
@@ -139,13 +172,13 @@ const SidePanel = (props) => {
           <span className="explorer">
             {workLang.explorer}
           </span>
-          <span className="new-folder" onClick={() => handleToast()}>
+          <span className="new-folder" onClick={() => handleToast("folder")}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="26" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
               <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z" />
               <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z" />
             </svg>
           </span>
-          <span className="new-file" onClick={() => fileCreation()}>
+          <span className="new-file" onClick={() => handleToast("file")}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="26" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
               <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z" />
               <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
@@ -170,9 +203,10 @@ const SidePanel = (props) => {
       </div>
       {
         toastShow === true ? (
-          <Toast show = {toastShow} message = {workLang.toastHeader} alignment = {"brew-toast-title"}
+          <Toast show = {toastShow} message = {folder ? workLang.toastFolderHeader : workLang.toastFileHeader} 
+          alignment = {"brew-toast-title"}
           handleClose = {() => handleToast()} data = {dataConfig} value = {value} error = {error}
-          node = {(data) => updateValue(data)} handleClick = {() => folderCreation()} />
+          node = {(data) => updateValue(data)} handleClick = {() => decideCreation()} />
         ) : (
           null
         )
